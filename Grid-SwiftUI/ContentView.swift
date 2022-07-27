0d3abb7883d8d4a6e1f3f307d37829c86913eece
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 17:35:51 -0700

    Initial Commit

---
+++ Grid-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grid-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Grid-SwiftUI/Grid_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_SwiftUIApp.swift
+//  Grid-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Grid_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
